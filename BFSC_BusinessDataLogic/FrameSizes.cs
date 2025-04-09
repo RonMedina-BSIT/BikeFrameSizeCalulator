a6027826e0bc0eb9215b9cb81682c3345f7ea8eb
RonPogi05 <roncyrusmedina@gmail.com>
2025-04-09 21:58:52 +0800

    Add project files.

---
+++ BFSC_BusinessDataLogic/FrameSizes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BFSC_BusinessDataLogic
+{
+    public class FrameSizes
+    {
+        public int TrekkingFrameSize { get; set; }
+        public int RoadFrameSize { get; set; }
+        public int MountainFrameSize { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿using System.Drawing;
+using BFSC_BusinessDataLogic;
+namespace BikeFrameSizeCalulator
+{
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("Welcome to Bike Frame Size Calulator");
+            AddandDelete();
+
+        }
+        public static void AddandDelete()
+        {
+            Console.Write("Please enter your inseam in cm: ");
+            int inseam = Convert.ToInt16(Console.ReadLine());
+
+            FrameSizes sizes =BFSC_Process.CalculateFrameSize(inseam);
+
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("Your bike frame size for: " + "\n" + "Trekking: " + sizes.TrekkingFrameSize + "\n" + "Road Bikes: " + sizes.RoadFrameSize + "\n" + "Mountain Bikes: " + sizes.MountainFrameSize);
+
+            Console.WriteLine("-------------------------");
+            Console.Write("Do you want to Delete Something?(Y/N): ");
+            string response = Console.ReadLine();
+            response = response.ToUpper();
+
+            if (response == "Y")
+            {
+                Console.WriteLine("-------------------------");
+                Console.WriteLine("Please select the item you want to delete: ");
+                string[] actions = new string[] { "[1] Trekking", "[2] Road Bikes", "[3] Mountain Bikes" };
+                Console.WriteLine("ACTIONS");
+
+                foreach (var action in actions)
+                {
+                    Console.WriteLine(action);
+                }
+                Console.WriteLine("-------------------------");
+                Console.Write("Enter Action: ");
+
+
+                int userAction = Convert.ToInt16(Console.ReadLine());
+
+                switch (userAction)
+                {
+                    case 1:
+                        Console.WriteLine("-------------------------");
+                        Console.WriteLine("Your bike frame size for: " + "\n" + "Road Bikes: " + sizes.RoadFrameSize + "\n" + "Mountain Bikes: " + sizes.MountainFrameSize);
+                        Update();
+                        break;
+                    case 2:
+                        Console.WriteLine("-------------------------");
+                        Console.WriteLine("Your bike frame size for: " + "\n" + "Trekking: " + sizes.TrekkingFrameSize + "\n" + "Mountain Bikes: " + sizes.MountainFrameSize);
+                        Update();
+                        break;
+                    case 3:
+                        Console.WriteLine("-------------------------");
+                        Console.WriteLine("Your bike frame size for: " + "\n" + "Trekking: " + sizes.TrekkingFrameSize + "\n" + "Road Bikes: " + sizes.RoadFrameSize);
+                        Update();
+                        break;
+                        default:
+                        Console.WriteLine("-------------------------");
+                        Console.WriteLine("Invalid input, please try again.");
+                        break;
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("-------------------------");
+                Console.WriteLine("Thank you for using the Bike Frame Size Calulator");
+                Console.WriteLine("Have a nice day!");
+                Update();
+            }
+        }
+        public static void Update()
+        {
+            Console.WriteLine("-------------------------");
+            Console.Write("Do you want to Change your inseam?(Y/N): ");
+            string response = Console.ReadLine();
+            response = response.ToUpper();
+
+            if (response == "Y")
+            {
+                AddandDelete();
+            }
+            else
+            {
+                Console.WriteLine("-------------------------");
+                Console.WriteLine("Thank you for using the Bike Frame Size Calulator");
+                Console.WriteLine("Have a nice day!");
+            }
+        }
+    }
+}
